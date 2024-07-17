185256796a189f48e28d05d1141840b70572d637
OCTER <to.ri.oct@icloud.com>
2024-07-17 14:44:11 +0900

    add arguments

--- main.py
+++ main.py
@@ -26,6 +26,9 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 def get_bus_info():
 
     options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
     options.add_experimental_option("prefs", {"download.default_directory": downloadplace, "download.prompt_for_download": False,  # ダウンロード時の確認ダイアログを表示しない
     "download.directory_upgrade": True,  # ダウンロードディレクトリの設定を有効化
     "plugins.always_open_pdf_externally": True  # PDF ファイルをブラウザで開かずに直接ダウンロードする
