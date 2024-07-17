2e2d81e8099a2f7a53324f739f7437dccc0b842e
OCTER <to.ri.oct@icloud.com>
2024-07-17 14:33:50 +0900

    fix

--- render-build.sh
+++ render-build.sh
@@ -9,7 +9,7 @@ if [ ! -d "$STORAGE_DIR/chrome" ]; then
   mkdir -p $STORAGE_DIR/chrome
   cd $STORAGE_DIR/chrome
   wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+  apt-get install -y ./google-chrome-stable_current_amd64.deb
   rm ./google-chrome-stable_current_amd64.deb
   cd /bot
 else
