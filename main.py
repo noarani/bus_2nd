a6ff208a282a807bed705282370d2922d35c00ef
OCTER <to.ri.oct@icloud.com>
2024-08-04 13:28:33 +0900

    /command実行時前回のembed delete

--- main.py
+++ main.py
@@ -37,10 +37,20 @@ def get_bus_info():
     "download.directory_upgrade": True,  # ダウンロードディレクトリの設定を有効化
     "plugins.always_open_pdf_externally": True  # PDF ファイルをブラウザで開かずに直接ダウンロードする
     })
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+    webdriver_path = ChromeDriverManager().install()
+    if os.path.splitext(webdriver_path)[1] != '.exe':
+        webdriver_dir_path = os.path.dirname(webdriver_path)
+        webdriver_path = os.path.join(webdriver_dir_path, 'chromedriver.exe')
+    chrome_service = Service(executable_path=webdriver_path)
+    driver = webdriver.Chrome(service=chrome_service, options=options)
 
     driver.minimize_window()
     driver.get('https://portal.mc.chitose.ac.jp/portal/?0')
+
+    wait = WebDriverWait(driver, 10)
+
+    wait.until(EC.presence_of_all_elements_located)
 
     idin = driver.find_element(By.XPATH, "//*[@id=\"userID\"]")
     pwin = driver.find_element(By.XPATH, "//*[@id=\"password\"]")
@@ -48,19 +58,24 @@ def get_bus_info():
     idin.send_keys(id)
     pwin.send_keys(pw)
 
-    driver.find_element(By.XPATH, "/html/body/div/div[4]/form/div[3]/div/input").click()#login
+    driver.find_element(By.CSS_SELECTOR, ".btn").click()#login
+    
 
-    wait = WebDriverWait(driver, 10)
+    
     wait.until(EC.presence_of_all_elements_located)
+
+    handoutbtn = driver.find_element(By.XPATH,"/html/body/div/div[1]/div[3]/ul/li[2]/a")
 
     notificationbtn = driver.find_element(By.XPATH, "/html/body/div/div[3]/div/div/div[2]/div/div[1]/div/div/div/div/dl[1]/dd/a/span")
     notificationbtn.click()#未読連絡開く//ok
 
     wait.until(EC.presence_of_all_elements_located)
 
+    file_name = "bus_schedule" + ".jpeg"
+
     try:
         driver.find_element(By.PARTIAL_LINK_TEXT, "シャトルバスダイヤについて").click()#「シャトルバスダイヤについて」の連絡を開く
-        wait.until(EC.presence_of_all_elements_located((By.XPATH, "/html/body/div/div[3]/div/div/div[2]/div/div/div/div[2]/div/table/tbody/tr[2]/td[2]/div/ul/li/a")))
+        wait.until(EC.presence_of_all_elements_located)
 
         delfiles = glob.glob(downloadplace+"\\*シャトルバス時刻表*.pdf")
         # 一致したファイルをすべて削除
@@ -71,31 +86,34 @@ def get_bus_info():
 
         bustimepdf = driver.find_element(By.XPATH, "/html/body/div/div[3]/div/div/div[2]/div/div/div/div[2]/div/table/tbody/tr[2]/td[2]/div/ul/li/a")
         bustimepdf.click()#download
-        time.sleep(10)
+
+        time.sleep(10)#ダウンロード待ち
         driver.quit()
         print('Downloaded new bus notification')
 
         # PDF to Image
-        pdf_files = glob.glob(downloadplace+"\\*シャトルバス時刻表*.pdf")
-        img_dir=Path(imageplace)
-        for pdf_path in pdf_files:
-            pages = convert_from_path(pdf_path, dpi=150)
+        pdf_file = glob.glob(downloadplace+"\\*シャトルバス時刻表*.pdf")
 
-        file_name = "bus_schedule" + ".jpeg"
+    # ここでpdf_fileがリストである場合、最初の要素を取得
+        if isinstance(pdf_file, list):
+            pdf_file = pdf_file[0]
+
+        img_dir=Path(imageplace)
+        page = convert_from_path(pdf_file, dpi=150)
+
         image_path = img_dir / file_name
-        pages[0].save(str(image_path), "JPEG")
+        page[0].save(str(image_path), "JPEG")
         print('Converted PDF to Image')
 
 
     except NoSuchElementException:
-        file_name = "bus_schedule" + ".jpeg"
+        
         # 要素が見つからない場合の処理(すでにダウンロード済み、画像もあるはず)
         print('Undefinde new bus notification')
         driver.quit()
         image_path = Path(imageplace)/file_name
 
     #この時点で画像できてる//ok
-    # ファイルをバイナリモードで開き、内容を読み込む
     return image_path
     
 
@@ -103,9 +121,11 @@ intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
+previous_message_id = None
+
 @client.event
 async def on_ready():
-    loop.start()
+    #loop.start()
     print("rdy")
     await tree.sync()#スラッシュコマンドを同期
 
@@ -140,12 +160,28 @@ async def loop():
 
 @tree.command(name="bus",description="bus-scheduleを表示します")
 async def bus_command(interaction: discord.Interaction):
+
+    global previous_message_id
+    
     embed = discord.Embed(title="シャトルバス時刻表", color=0x00ff00)
     fname = "bus_schedule.jpeg"
     await interaction.response.defer()
     file = discord.File(get_bus_info(), filename = fname, spoiler = False)
     embed.set_image(url = "attachment://" + fname)
-    await interaction.followup.send( file = file,embed = embed)
+
+    if previous_message_id:
+        try:
+            previous_message = await interaction.channel.fetch_message(previous_message_id)
+            await previous_message.delete()
+        except discord.NotFound:
+            pass  # メッセージが見つからない場合は無視
+
+    # 新しいメッセージを送信
+    message = await interaction.followup.send(file=file, embed=embed)
+    # 新しいメッセージIDを保存
+    previous_message_id = message.id
+
+    
 
 
                     
