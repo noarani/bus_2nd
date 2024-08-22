e999fe0131534d6f6b99b1aa585c2640c7f1d607
OCTER <to.ri.oct@icloud.com>
2024-08-22 13:23:25 +0900

    add cleanup

--- main.py
+++ main.py
@@ -184,7 +184,15 @@ async def bus_command(interaction: discord.Interaction):
     # 新しいメッセージIDを保存
     previous_message_id = message.id
 
-    
+@client.event
+async def on_message(message):
+    if message.content == '/cleanup':
+        if message.author.guild_permissions.administrator:
+            await message.channel.purge()
+            await message.channel.send('塵一つ残らないね！')
+        else:
+            await message.channel.send('何様のつもり？')
+
 
 
                     
